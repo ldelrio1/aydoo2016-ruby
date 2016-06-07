b1ec2961c68c07c19801c8745dd0164e1204c415
ldelrio1 <lucasdr.lgi@gmail.com>
2016-06-07 16:39:40 -0300

    Agrega Formato de Salida -> Quiet

--- factores-primos-3/model/formato_de_salida.rb
+++ factores-primos-3/model/formato_de_salida.rb
@@ -6,6 +6,11 @@ class FormatoDeSalida
     @salida_pretty << pretty.join(" ").to_s
 
   end
+  
+  def quiet(factores_primos)
+    @quiet = factores_primos
+    @quiet.join("\n")
 
+  end
 
 end
--- factores-primos-3/spec/formato_de_salida_spec.rb
+++ factores-primos-3/spec/formato_de_salida_spec.rb
@@ -9,5 +9,11 @@ describe 'FormatoDeSalida' do
     expect(formato.pretty(10, array_entrante)).to eq 'Factores Primos 10: 2 5'
   end
 
+  it 'espera quiet' do
+    formato = FormatoDeSalida.new
+    array_entrante = [2,5,7]
+    salida_quiet = "2\n5\n7"
+    expect(formato.quiet(array_entrante)).to eq salida_quiet
+  end
 
 end
